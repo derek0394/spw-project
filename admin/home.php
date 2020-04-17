47c5d428bf33889528e189adca398103efc1a2de
derek noronha <francisderek3@gmail.com>
2020-04-17 15:36:34 +0100

    update

--- admin/home.php
+++ admin/home.php
@@ -19,9 +19,15 @@ if (isset($_GET['logout'])) {
 <head>
 	<title>Home</title>
 	<link rel="stylesheet" type="text/css" href="../style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 	<style>
+		h2{
+			text-align: center;
+		}
 	.header {
-		background: #003366;
+		background: #d08722;
 	}
 	button[name=register_btn] {
 		background: #003366;
@@ -30,7 +36,7 @@ if (isset($_GET['logout'])) {
 </head>
 <body>
 	<div class="header">
-		<h2>Admin - Home Page</h2>
+		<h2>Admin Dashboard</h2>
 	</div>
 	<div class="content">
 		<!-- notification message -->
@@ -47,20 +53,101 @@ if (isset($_GET['logout'])) {
 
 		<!-- logged in user information -->
 		<div class="profile_info">
-			<img src="../images/admin_profile.png"  >
-
+			
+		<div class="container">
 			<div>
 				<?php  if (isset($_SESSION['user'])) : ?>
-					<strong><?php echo $_SESSION['user']['username']; ?></strong>
+					<h4><strong><?php echo strtoupper($_SESSION['user']['username']); ?></strong>
 
 					<small>
 						<i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i> 
 						<br>
 						<a href="home.php?logout='1'" style="color: red;">logout</a>
-                       &nbsp; <a href="create_user.php"> + add user</a>
-					</small>
+                       &nbsp; <a href="create_user.php"> + ADD NEW USER/ADMIN</a>
+                       &nbsp; <a href="delete-user.php"> x DELETE USER</a>
+					</small></h4>
 
 				<?php endif ?>
+			</div>
+		</div>
+			<div class="container">
+				<h4>Users tried to access admin page (Recent)</h4>
+				<table class="table">
+				    <thead>
+				       <tr>
+				        <th>USERNAME</th>
+				        <th>DATE & TIME</th>
+				        <th>IP ADDRESS</th>
+				       </tr>
+				    	<?php
+				    	$query_details = "SELECT * FROM tried_to_access_admin_page ORDER BY id DESC";
+						$result = mysqli_query($conn, $query_details);
+						if ($result->num_rows > 0) 
+						{
+							while($row = $result->fetch_assoc()) 
+							{
+								$date_time_from_DB[] = $row['date_time'];
+								$username_from_DB[] = $row['username'];
+								$ip_address_from_DB[] = $row['date_time'];			
+							}
+						}
+
+				    	 for($i=0; $i<sizeof($username_from_DB); $i++)
+				    	{ ?>
+ 					   <tr>
+				        <td><?php echo $username_from_DB[$i]; ?></td>
+				        <td><?php echo $date_time_from_DB[$i]; ?></td>
+				        <td><?php echo $ip_address_from_DB[$i]; ?></td>
+				      </tr>
+				        <?php
+				     
+				    	} ?>
+				     
+				    </thead>
+				    <tbody>
+				     
+				    </tbody>
+				 </table>
+			</div>
+
+			<div class="container">
+				<h4>Wrong User Logins (Recent)</h4>
+				<table class="table">
+				    <thead>
+				       <tr>
+				        <th>USERNAME</th>
+				        <th>DATE & TIME</th>
+				        <th>IP ADDRESS</th>
+				       </tr>
+				    	<?php
+				    	$query_details = "SELECT * FROM wrong_user_logs ORDER BY id DESC";
+						$result = mysqli_query($conn, $query_details);
+						if ($result->num_rows > 0) 
+						{
+							while($row = $result->fetch_assoc()) 
+							{
+								$date_time_from_DB[] = $row['date_time'];
+								$username_from_DB[] = $row['username'];
+								$ip_address_from_DB[] = $row['date_time'];			
+							}
+						}
+
+				    	 for($i=0; $i<sizeof($username_from_DB); $i++)
+				    	{ ?>
+ 					   <tr>
+				        <td><?php echo $username_from_DB[$i]; ?></td>
+				        <td><?php echo $date_time_from_DB[$i]; ?></td>
+				        <td><?php echo $ip_address_from_DB[$i]; ?></td>
+				      </tr>
+				        <?php
+				     
+				    	} ?>
+				     
+				    </thead>
+				    <tbody>
+				     
+				    </tbody>
+				 </table>
 			</div>
 		</div>
 	</div>
