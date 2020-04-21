52c2b896d8975af567453ef0a117cb4e7faa40b7
derek noronha <francisderek3@gmail.com>
2020-04-21 22:18:23 +0100

    update

--- database_connection.php
+++ database_connection.php
@@ -6,12 +6,12 @@ $pass = "";
 $database = "application";
 */
 
-/*
+
 $servername = "dyud5fa2qycz1o3v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $user1 = "ulo9xf8ad5xednpc";
 $pass = "mmlbrz7cagzqssyg";
 $database = "itrdhg03zk2ikdkj";
-*/
+
 
 // Create connection
 $conn = new mysqli($servername, $user1, $pass, $database);
